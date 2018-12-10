71e564c422b64567a3cc65ff806a6fd9462f4915
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-10 10:32:09 -0800

    Edit users reducer

--- benchbnb/frontend/reducers/users_reducer.js
+++ benchbnb/frontend/reducers/users_reducer.js
@@ -3,11 +3,9 @@ import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
 
 export default (oldState = {}, action) => {
     Object.freeze(oldState);
-    let newState = merge({}, oldState);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            newState[action.currentUser.id] = action.currentUser;
-            return newState;
+            return merge({}, oldState, { [action.currentUser.id]: action.currentUser});
         default:
             return oldState;
     }
