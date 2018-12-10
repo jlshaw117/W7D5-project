c9ae67c59ce04a9f55a4cbd5c1ce53ad3abb86d5
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-10 11:03:43 -0800

    Add app.jsx and root.jsx to components and import into entry file

--- benchbnb/frontend/bench_bnb.jsx
+++ benchbnb/frontend/bench_bnb.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import { login, logout, signup} from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  const store = configureStore();
+  ReactDOM.render(<Root store={store}/>, root);
 
   //testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
   window.signup = signup;
